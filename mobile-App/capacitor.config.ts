4b3ecdb1b1bb5bdb38817ad8ee28e6b37c47a318
Khat <153098036+khat001@users.noreply.github.com>
2025-09-07 21:40:46 +0800

    Delete mobile-App directory

--- mobile-App/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'meowskin.sample.app',
-  appName: 'MeowSkin App',
-  webDir: 'www'
-};
-
-export default config;
