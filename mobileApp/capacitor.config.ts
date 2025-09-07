9c36c971840e9163202c4a717a639a9bcb0a3af6
Khat <153098036+khat001@users.noreply.github.com>
2025-09-07 21:46:23 +0800

    Add files via upload

---
+++ mobileApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'meowskin.sample.app',
+  appName: 'MeowSkin App',
+  webDir: 'www'
+};
+
+export default config;
